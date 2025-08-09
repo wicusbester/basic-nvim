f41afd9c27def868080e5ab089653d0e5d2dbbc0
Johannes Lodewicus Bester <27904024@sun.ac.za>
2025-08-09 14:54:53 +0200

    added

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
 -- Project spec setup
 vim.opt.textwidth = 80
 vim.opt.tabstop = 4
@@ -6,16 +9,27 @@ vim.opt.autoindent = true
 
 
 -- My stuff
-vim.opt.termguicolors = true
-vim.opt.clipboard = 'unnamedplus'
+-- vim.opt.termguicolors = true
 vim.opt.scrolloff = 10
 vim.opt.inccommand = 'split'
+
+vim.opt.colorcolumn = '80'
 
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+vim.opt.mouse = 'a'
+
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
+vim.opt.undofile = true
+
+
 
 vim.cmd('filetype plugin on')
 vim.cmd('syntax on')
